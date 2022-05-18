8305f6408194aea374eaabe9d061b89b6082dca6
rajni <sainirajni292000@gmail.com>
2022-05-18 14:29:32 +0530

    updated

--- src/components/admin-dashboard/AdminSigninFrom.jsx
+++ src/components/admin-dashboard/AdminSigninFrom.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Form from 'react-bootstrap/Form'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/button'
+import Button from 'react-bootstrap/Button'
 import axios from "axios";
 // import { displayUser } from "../../redux/signin/signin.action";
 // import { useNavigate } from "react-router-dom";
--- src/components/admin-dashboard/AdminSignupFrom.jsx
+++ src/components/admin-dashboard/AdminSignupFrom.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import Form from 'react-bootstrap/Form'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Button from 'react-bootstrap/button'
+import Button from 'react-bootstrap/Button'
 
 
 
